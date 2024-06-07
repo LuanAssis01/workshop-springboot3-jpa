5272ffa6c4e43cca3d94dba44bcd0f4944097583
LuanAssis01 <assisluan863@gmail.com>
2024-06-06 21:53:35 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResources.java
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,11 @@ public class UserResources {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
-    } 
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.updateUser(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
